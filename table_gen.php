4c6f6eadce15ac0cb1b1ea555a2467f05e010c4d
Wan Jun <D1124182712@gm.lhu.edu.tw>
2024-10-08 11:54:29 +0800

    加了下拉式表格跟單選標籤

--- table_gen.php
+++ table_gen.php
@@ -1,9 +1,14 @@
 <?php
     $row_count = $_GET['row_num'];
     $col_count = $_GET['col_num'];
+    $gender = $_GET['gender'];
+    $dept = $_GET['department'];
+    $mes = $_GET['message'];
    
-    echo '表格大小:'. $row_count .'列，'. $col_count .'行。';
-    
+    echo '表格大小:'. $row_count .'列，'. $col_count .'行。<br/>' ;
+    echo '性別:' . $gender . '<br/>';
+    echo '科系:'. $dept . '<br/>';
+    echo '留言:'. $mes;
     echo '<table border="1">';
 
     for ($i = 0; $i < $row_count; $i++) {
